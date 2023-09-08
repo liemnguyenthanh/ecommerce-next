99268886858f8678d3ba0b100f37e3625755d4c5
liem_nguyen_thanh <liemdonduong@gmail.com>
2023-09-08 09:46:51 +0700

    feat: config husky

--- src/components/common/box/index.tsx
+++ src/components/common/box/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
 const CustomBox = () => {
-  const handle = () => {const a = 1};
+  const handle = () => {
+    const a = 1;
+  };
   return <div className=''>a</div>;
 };
 
