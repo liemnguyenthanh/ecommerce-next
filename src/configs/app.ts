5081b307dc0e98a7cde85f41feebda02adcede19
liem_nguyen_thanh <liemdonduong@gmail.com>
2023-09-11 20:31:08 +0700

    chore: remove api necessary

--- src/configs/app.ts
+++ src/configs/app.ts
@@ -1,4 +1,3 @@
 export const AppConfig = {
   apiBase: process.env.NEXT_PUBLIC_API_BASE ?? 'https://fakestoreapi.com/',
-  enableApiMockup: !!parseInt(process.env.NEXT_PUBLIC_ENABLE_API_MOCKUP || '0'),
 };
