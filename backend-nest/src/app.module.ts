5c501a6f7c35ba2a604980203c3e59b3154f1921
adatdat <1712328@student.hcmus.edu.vn>
2023-09-14 22:23:08 +0900

    feat(mongoose): [d41sy] add design pattern for users & docker of mongoDB

--- backend-nest/src/app.module.ts
+++ backend-nest/src/app.module.ts
@@ -4,27 +4,48 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { database_config } from './configs/configuration.config';
 import * as Joi from 'joi';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigService } from '@nestjs/config';
+import { UsersModule } from '@modules/users/users.module';
+import { AuthModule } from '@modules/auth/auth.module';
+import { UserRolesModule } from '@modules/user-roles/user-roles.module';
 
 @Module({
-	imports: [
-		ConfigModule.forRoot({
-			isGlobal: true,
-			envFilePath: process.env.NODE_ENV === 'development' ? '.env.dev' : '.env',
-			load: [database_config],
-			cache: true,
-			expandVariables: true,
-			validationSchema: Joi.object({
-				NODE_ENV: Joi.string()
-					.valid('development', 'production', 'test', 'provision', 'staging')
-					.default('development'),
-				PORT: Joi.number().default(3000),
-			}),
-			validationOptions: {
-				abortEarly: false,
-			},
-		}),
-	],
-	controllers: [AppController],
-	providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV === 'development' ? '.env.dev' : '.env',
+      load: [database_config],
+      cache: true,
+      expandVariables: true,
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+          .valid('development', 'production', 'test', 'provision')
+          .default('development'),
+        PORT: Joi.number().port().required(),
+        DATABASE_PORT: Joi.number().port().required(),
+        DATABASE_USERNAME: Joi.string().min(4).required(),
+        DATABASE_PASSWORD: Joi.string().min(4).required(),
+        DATABASE_HOST: Joi.string().required(),
+        DATABASE_URI: Joi.string().required(),
+      }),
+      validationOptions: {
+        abortEarly: false,
+      },
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('DATABASE_URI'),
+        dbName: configService.get<string>('DATABASE_NAME'),
+      }),
+      inject: [ConfigService],
+    }),
+    UsersModule,
+    AuthModule,
+    UserRolesModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
